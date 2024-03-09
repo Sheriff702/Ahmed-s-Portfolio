4c27578a3485bd067b10cb2de39880032aba8554
Ahmed Sharif <144018452+Sheriff702@users.noreply.github.com>
2024-03-09 13:30:08 +0100

    Portfolio

---
+++ src/content_option.js
@@ -0,0 +1,199 @@
+import img from "./me5.png";
+
+const logotext = "AHMED SHARIF";
+const meta = {
+  title: "Ahmed Sharif",
+  description:
+    "I’m Ahmed Sharif and im a frontend developer and designer. I love coding and i code cool websites. I develop mobile apps. I'm the guy that everyone wants to work with because i'm so cool and i can code cool websites.",
+};
+
+const introdata = {
+  title: "I’m Ahmed Sharif",
+  animated: {
+    first: "I Love Coding",
+    second: "I Code Cool Websites",
+    third: "I Develop Mobile Apps",
+    forth: "I'm Just Cool",
+  },
+  description:
+    "I'm the guy that everyone wants to work with because i'm so cool and i can code cool websites.",
+  your_img_url: img,
+};
+
+const dataabout = {
+  title: "About Moi",
+  aboutme:
+    "👋  Hey, I'm Ahmed Sharif - a 30-year-old guy with a passion for innovation and creativity. Currently, I'm rocking the role of Chief Technology Officer (CTO) at [Your Company], a venture I co-founded to bring groundbreaking ideas to life.",
+
+  aboutme2:
+    "🚀 My journey has been anything but conventional. While my days are filled with coding and leading tech initiatives, my nights used to resonate with beats as I spent 13 memorable years as a DJ. Music has been an integral part of my life, shaping not only my evenings but also influencing my approach to problem-solving and creativity.",
+  aboutme3:
+    "🎓 I'm currently deep-diving into the world of Frontend Development at Hyper Island, where I'm honing my skills to create seamless and user-friendly digital experiences. The blend of my technical expertise and creative background gives me a unique perspective in navigating the ever-evolving landscape of technology.",
+  aboutme4:
+    "🌐 Over the years, I've embraced a myriad of roles and experiences. From spinning tracks in lively venues to steering a tech startup, every chapter has contributed to the diverse skill set and mindset I bring to the table.",
+};
+const worktimeline = [
+  {
+    jobtitle: "CTO & Co-Founder",
+    where: "The Wiggle App(Local le Swued AB)",
+    date: "2023 - Present",
+  },
+  {
+    jobtitle: "Production Manager",
+    where: "Örebro Län",
+    date: "2017 - 2021",
+  },
+  {
+    jobtitle: "Dj",
+    where: "Worldwide",
+    date: "2011 - Present",
+  },
+];
+
+const skills = [
+  {
+    name: "HMTL & CSS",
+    value: 100,
+  },
+  {
+    name: "Javascript",
+    value: 90,
+  },
+  {
+    name: "React",
+    value: 75,
+  },
+  {
+    name: "node.js",
+    value: 60,
+  },
+  {
+    name: "Photoshop",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Design",
+    description:
+      "Passionate about crafting intuitive and visually appealing user interfaces. I believe in creating seamless user experiences that leave a lasting impression. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Mobile Apps",
+    description:
+      "Bringing ideas to life through cutting-edge mobile app development. From concept to deployment, I specialize in building responsive and feature-rich applications that cater to modern user needs. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+  {
+    title: "Custom Web Solutions",
+    description:
+      "Crafting unique and tailored web solutions to meet your specific needs. Whether it's a dynamic website or a complex web application, I specialize in delivering custom solutions that stand out. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "Ahmedsheriffbooking@gmail.com",
+  YOUR_FONE: "(+46)076 066 47 54",
+  description:
+    "Drop me a line if you have any questions or just want to say hi. I'll get back to you as soon as possible!",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_c3fx2b8",
+  YOUR_TEMPLATE_ID: "template_9nus7zw",
+  YOUR_USER_ID: "uDfnGgGBD0zBBIZU0",
+};
+
+const socialprofils = {
+  github: "https://github.com/Sheriff702",
+  facebook: "https://www.facebook.com/poetens",
+  linkedin: "https://www.linkedin.com/in/sheriff702/",
+  twitter: "https://twitter.com/svarta_shunon",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
